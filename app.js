122e2e9ab279740deec0f5899e3f75b0c49414cf
Peter Osoo <posoo617@gmail.com>
2023-01-11 14:38:34 +0300

    numbers

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+// numbers
+const number = 23
+console.log(number)
+// number is blue in the console while string is black
+
 let my_name = "Rateng D"
 console.log(my_name)
 console.log(my_name)
-console.log(my_name)
+console.log(my_name, my_name)
 
 let school, subjects, teacher
 
