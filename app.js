c0195818641cd89187b8d6b10a85ce21fca14539
Peter Osoo <posoo617@gmail.com>
2023-01-11 14:21:33 +0300

    assign variables

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ console.log(my_name)
 console.log(my_name)
 console.log(my_name)
 
+let school, subjects, teacher
+
+school = "Maranda High"
+subjects = ["CRE", "HTML", "CSS"]
+console.log(school)
+console.log(subjects)
+
 // console.log("Rateng")
 // document.write("Hello App")
 // console.log({ name: "Jakablack" })
