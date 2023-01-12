dbcb9b19dba172fe67019f91275ac6cea1fdea48
Peter Osoo <posoo617@gmail.com>
2023-01-12 09:03:03 +0300

    numbers

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
-// numbers
-const number = 23
-console.log(number)
-// number is blue in the console while string is black
-
-let my_name = "Rateng D"
-console.log(my_name)
-console.log(my_name)
-console.log(my_name, my_name)
-
-let school, subjects, teacher
-
-school = "Maranda High"
-subjects = ["CRE", "HTML", "CSS"]
-console.log(school)
-console.log(subjects)
-
-// console.log("Rateng")
-// document.write("Hello App")
-// console.log({ name: "Jakablack" })
+const cones = 10
+const games = 3
+const amount = cones % games
+console.log(amount)
---
+++ app1.js
@@ -0,0 +1,24 @@
+// numbers
+const number = 23
+console.log(number)
+// number is blue in the console while string is black
+
+let my_name = "Rateng D"
+console.log(my_name)
+console.log(my_name)
+console.log(my_name, my_name)
+
+let school, subjects, teachers
+
+school = "Maranda High"
+subjects = ["CRE", "HTML", "CSS"]
+teachers = { Jack: "Math", Jakablak: " Science" }
+
+console.log(school)
+console.log(subjects)
+console.log(teachers)
+console.log(teachers.Jack)
+
+// console.log("Rateng")
+// document.write("Hello App")
+// console.log({ name: "Jakablack" })
