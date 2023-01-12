999da0f36aee4031d3ec66711450c15fa6010122
Peter Osoo <posoo617@gmail.com>
2023-01-12 09:26:18 +0300

    functions

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const cones = 10
-const games = 3
-const amount = cones % games
-console.log(amount)
+// Functions - declare, invoke
 
-console.log(typeof null)
+function greetings() {
+	console.log("Hello Mine")
+	console.log("Hello Universe")
+	console.log("Hello World")
+}
+
+greetings()
+greetings()
+greetings()
+
+// Arrays
+
+const friends = ["Anna", "Nita", "Bali", " Sue"]
+console.log(friends)
+console.log(friends[1])
+
+let bestFriend = friends[2]
+console.log(bestFriend)
+// const cones = 10
+// const games = 3
+// const amount = cones % games
+// console.log(amount)
+
+// console.log(typeof null)
