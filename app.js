203e5e608cc4c6b1ab50318453e37f87dc677bcb
Peter Osoo <posoo617@gmail.com>
2023-01-16 13:35:54 +0300

    logical operators - or

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const num1 = 3
+const num1 = 13
 const num2 = 5
 
 if (num1 > num2) {
 	console.log("Num 1 is bigger")
 } else {
 	console.log("Num 2 is bigger")
+}
+
+const value = false
+
+if (!value) {
+	console.log("Value is true")
+}
+
+const fname = "Peter"
+const age = 24
+
+if (fname === "Bob" || age === 25) {
+	console.log("Hello there user")
+} else {
+	console.log("Wrong Values")
 }
