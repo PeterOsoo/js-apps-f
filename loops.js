5ec97eb09c9edbbb8add00b81796c8d3fe7c1135
Peter Osoo <posoo617@gmail.com>
2023-01-17 10:41:54 +0300

    loops

---
+++ loops.js
@@ -0,0 +1,10 @@
+let money = 0
+
+do {
+	console.log("You have " + money + " Kshs")
+	money++
+} while (money < 11)
+
+for (let number = 0; number < 11; number++) {
+	console.log("This is Number " + number)
+}
