f97f4baf3c9a8676ab82aa65d5476e72fd422eab
Peter Osoo <posoo617@gmail.com>
2023-01-17 10:37:09 +0300

    do while loop

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
-const num1 = 13
-const num2 = 5
+let money = 0
 
-if (num1 > num2) {
-	console.log("Num 1 is bigger")
-} else {
-	console.log("Num 2 is bigger")
-}
-
-const value = false
-
-if (!value) {
-	console.log("Value is true")
-}
-
-const fname = "Peter"
-const age = 24
-
-if (fname === "Bob" || age === 25) {
-	console.log("Hello there user")
-} else {
-	console.log("Wrong Values")
-}
+do {
+	console.log("You have " + money + " Kshs")
+	money++
+} while (money < 11)
