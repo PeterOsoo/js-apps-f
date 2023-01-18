4921c09ad855b766136b901b6609efc7cb00dcf1
Peter Osoo <posoo617@gmail.com>
2023-01-18 12:40:35 +0300

    calculate total

---
+++ totals.js
@@ -0,0 +1,16 @@
+const gas = [45, 67, 80]
+const food = [30, 30, 40]
+
+calculateTotal = arr => {
+	let total = 0
+	for (let i = 0; i < arr.length; i++) {
+		total += arr[i]
+	}
+	return total
+}
+
+const gasTotal = calculateTotal(gas)
+const foodTotal = calculateTotal(food)
+
+console.log(gasTotal)
+console.log(foodTotal)
