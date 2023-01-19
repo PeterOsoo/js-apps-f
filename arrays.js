31878b4d92eda7387fa558deb79b5b5be1d4f6f4
Peter Osoo <posoo617@gmail.com>
2023-01-19 13:18:46 +0300

    for each

---
+++ arrays.js
@@ -0,0 +1,16 @@
+// Powerful Array Methods
+
+// foreach
+// does not return array
+
+const people = [
+	{ name: "bob", age: 45, position: "designer" },
+	{ name: "Jaka", age: 25, position: "developer" },
+	{ name: "Sue", age: 22, position: "make-up" },
+]
+
+function showPerson(person) {
+	console.log(person)
+}
+
+people.forEach(showPerson)
