19085dd9692935549f4c5413c20212a63176dc29
Peter Osoo <posoo617@gmail.com>
2023-01-23 19:44:34 +0300

    add h2 - create Element

--- Append/app.js
+++ Append/app.js
@@ -5,3 +5,12 @@ const bodyDiv = document.createElement("div")
 const text = document.createTextNode("This text comes from my Js file")
 bodyDiv.appendChild(text)
 result.appendChild(bodyDiv)
+
+const heading = document.createElement("h2")
+
+const headingText = document.createTextNode(
+	"This text comes from my Js file for heading"
+)
+heading.appendChild(headingText)
+heading.classList.add("blue")
+result.appendChild(heading)
