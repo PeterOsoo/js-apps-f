cd0846c172df885edb7aa166ccea065310301962
Peter Osoo <posoo617@gmail.com>
2023-01-23 15:11:21 +0300

    querySelectorAll

--- DOM/app.js
+++ DOM/app.js
@@ -6,19 +6,27 @@ const h3 = document.getElementById("h3")
 h3.style.color = "blue"
 
 const headings = document.getElementsByTagName("h3")
-console.log(headings)
+// console.log(headings)
 
 headings[1].style.color = "green"
 
 const listItems = document.getElementsByClassName("special")
-console.log(listItems)
+// console.log(listItems)
 
 listItems[2].style.color = "gray"
 
 // querySelector
 const result = document.querySelector("#result")
-console.log(result)
+// console.log(result)
 result.style.color = "orange"
 
 const item = document.querySelector(".special")
-console.log(item)
+// console.log(item)
+
+const list = document.querySelectorAll(".normal")
+// console.log(list)
+
+list.forEach(function (item) {
+	item.style.color = "blue"
+	console.log(item)
+})
